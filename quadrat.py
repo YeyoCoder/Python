0a523bda89883ca0f614915799cba34eca61858c
Diego Olvera <olveram.diego@outlook.com>
2019-01-30 13:57:17 -0600

    Input function added and NonZeroDivision Exception add

--- quadrat.py
+++ quadrat.py
@@ -5,31 +5,34 @@ Created on Fri Jan 25 09:16:55 2019
 @author: olver
 """
 import math
+
+def input_float(string):
+    while True:
+        try:
+            return float(input(string))
+            break
+        except ValueError:
+            print("Introduce un número de punto flotante")
+            
 print("Resolver ax^2 + bx + c = 0")
-while True:
-    try:
-        a=float(input("a = "))
-        break
-    except ValueError:
-        print("Introduce un número de punto flotante")
-while True:
-    try:
-        b=float(input("b = "))
-        break
-    except ValueError:
-       print("Introduce un número de punto flotante")
-while True:
-    try:
-        c=float(input("c = "))
-        break
-    except ValueError:
-        print("Introduce un número de punto flotante")
-        
-d = b**2-4 - 4*a*c
+a = input_float("a = ")
+b = input_float("b = ")
+c = input_float("c = ")
+
+d = b**2 - 4*a*c
 if d >= 0:
-    x1=(-b+math.sqrt(d)) / (2* a)
-    x2=(-b-math.sqrt(d)) / (2* a)
-    print("x1 = {}, x2 = {}".format(x1,x2))
+    try:
+        x1=(-b+math.sqrt(d)) / (2* a)
+        x2=(-b-math.sqrt(d)) / (2* a)
+        print("x1= {}, x2 = {}".format(x1,x2))
+    except ZeroDivisionError:
+        try:
+            print("x={}".format(-c/ b))
+        except ZeroDivisionError:
+            if c == 0:
+                print("Hay infinitas soluciones")
+            else:
+                print("No hay solución")                
 else:
     re = -b / (2 * a)
     im = math.sqrt(-d) / (2 * a)    
